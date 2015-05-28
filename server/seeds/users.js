c14de8dd6ef00c797ba8680e02be4efbf5c2f7df
Matt Berry <git@mjberry.com>
2015-05-28 16:48:59 -0500

    added semicolon

--- server/seeds/users.js
+++ server/seeds/users.js
@@ -1,7 +1,7 @@
 Meteor.startup(function() {
 
     if (Meteor.users.find({}).count() === 0) {
-        Accounts.createUser({username: 'mjberry@illinois.edu', email: 'mjberry@illinois.edu', password: 'changeme', profile: {name: 'Matt Berry'}})
+        Accounts.createUser({username: 'mjberry@illinois.edu', email: 'mjberry@illinois.edu', password: 'changeme', profile: {name: 'Matt Berry'}});
     }
 
 });

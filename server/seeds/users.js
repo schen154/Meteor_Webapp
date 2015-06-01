3615c80bfb9439dd073e0d2d0a0da587bfd10ec4
Suyang Chen <schen154@illinois.edu>
2015-06-01 09:47:38 -0500

    Suyang added account

--- server/seeds/users.js
+++ server/seeds/users.js
@@ -2,6 +2,8 @@ Meteor.startup(function() {
 
     if (Meteor.users.find({}).count() === 0) {
         Accounts.createUser({username: 'mjberry@illinois.edu', email: 'mjberry@illinois.edu', password: 'changeme', profile: {name: 'Matt Berry'}});
+        Accounts.createUser({username: 'schen154@illinois.edu', email: 'schen154@illinois.edu', password: 'chsy961022', profile: {name: 'Suyang Chen'}});
+
     }
 
 });

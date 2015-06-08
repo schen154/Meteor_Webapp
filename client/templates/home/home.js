29a1b23818e7b7e3940ae4fdde469cfab0377ac3
Suyang Chen <schen154@illinois.edu>
2015-06-08 16:42:16 -0500

    client/templates/home/home.html added sidebar

--- client/templates/home/home.js
+++ client/templates/home/home.js
@@ -1,3 +1,15 @@
 Template.home.rendered = function() {
 
 };
+Template.home.events({
+    'click #slide-menu': function(){
+        $('.list-group').fadeOut(10);
+        $('.mini-menu').fadeIn();
+    },
+    'click .mini-menu': function(){
+        $('.mini-menu').fadeOut();
+        $('.list-group').fadeIn();
+    }
+
+
+});

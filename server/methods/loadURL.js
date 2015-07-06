34bf31f3f69ce521f7d61cc89164c30095a62d59
Matt Berry <git@mjberry.com>
2015-07-06 10:53:57 -0500

    fixed call to FS.Store

--- server/methods/loadURL.js
+++ server/methods/loadURL.js
@@ -1,7 +1,6 @@
 Meteor.methods({
     grabDataFile: function(url) {
         // Make synchronous http call
-        console.log("This doesn't print out!!  T^T");
         var result = Meteor.http.get(url, {timeout: 30000});
         if (result.statusCode != 200) {
             console.log("Response issue: ", result.statusCode);
@@ -14,8 +13,14 @@ Meteor.methods({
         // Create the FS.File instance
         var newFile = new FS.File();
 
+        // Copy the data to a Uint8Array (attachData accepts strings but expects them to be URLs)
+        var uinta = new Uint8Array(result.content.length);
+        for(var i=0, j=result.content.length; i<j; i++) {
+            uinta[i] = result.content.charCodeAt(i);
+        }
+
         // Attach the parsed data file to it.
-        newFile.attachData(result.content, {type: 'text/plain'});
+        newFile.attachData(uinta, {type: 'text/plain'});
 
         //Get filename from URL
         var origFileName = url.substring(url.lastIndexOf('/')+1);
@@ -24,7 +29,7 @@ Meteor.methods({
         var currDate = (new Date()).valueOf();
 
         // Give it a unique file name
-        newFile.name(origFileName+CurrDate+'.txt');
+        newFile.name(origFileName+currDate+'.txt');
 
         // `dataFiles` is an `FS.Collection` instance defined in *FileStorage.js*
         dataFiles.insert(newFile);
